86f5c1a2bdb6a78b50ee5e6c9011e0507735fda9
street-07 <ujjwalmehta6969@gmail.com>
2025-03-31 14:57:56 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,30 @@
+"use strict "; //this keyword treat your all js code as new version
+
+//alert(3+3)   // we are using nodejs , not browser(engine)
+
+console.log(3+3) ; // code readilble should follow,documentation mdn,tc39
+
+console.log("hitesh");
+
+//data type
+
+let name="hitish" // string dt
+let age=14;
+let isLoggedIn=false
+
+//number =>2 to pow53
+//string =>""
+//bigint
+//boolean=>true or false
+//null=> standalone value (represent a empty vale)
+//undefined => 
+// symbol => unique
+
+
+//object
+
+console.log(typeof age);
+
+console.log(typeof undefined);    //undefined
+console.log(typeof null);     //object
+
