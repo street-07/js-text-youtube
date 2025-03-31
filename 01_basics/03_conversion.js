8598fc3f3ce4f8b1e50a5f9e007fbb4eee2d2853
street-07 <ujjwalmehta6969@gmail.com>
2025-03-31 19:16:18 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,41 @@
+let score = true
+
+// console.log(typeof score);
+
+ let alue=Number(score)   // write side always in caps,convert string to number
+// console.log(typeof alue);
+
+// console.log(alue);
+
+
+/*
+// "33"=> 33
+//"33abc"=> NaN(not a number) converted but not give value
+// true => 1 
+
+// convert in boolean
+*/
+
+
+let isLogged="ram";
+
+// let change=Boolean(isLogged);
+
+// console.log(typeof change);
+// console.log(change);
+
+//1=>true,0=>false
+//""=>false
+//"ram"=>true
+
+
+let check=33
+
+console.log(typeof check);
+
+let change=String(check)
+
+console.log(typeof change);
+console.log(change);
+
+
