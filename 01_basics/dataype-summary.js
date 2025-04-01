25e764457ba9ff5325dee489f72ce1cfc7463899
street-07 <ujjwalmehta6969@gmail.com>
2025-04-01 19:39:03 +0000

    datatype-summary

---
+++ 01_basics/dataype-summary.js
@@ -0,0 +1,43 @@
+//primitve
+
+//7types : string , number , boolean , null(empty), undefined , symbol
+//bigint 
+
+const score = 100 // dynamically typed becoz not required bool or int 
+
+const isLoggedIn = false 
+const temp= null;
+let email = undefined ;
+
+//declare symbol
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+// big number
+const bigNumber = 32458796522155866445822n
+
+
+
+
+// reference(non primtive)
+
+// array , object, function, 
+
+
+const heros =["shaktiman","naagraj","doga"]
+//object
+
+let myobj = {
+    name: "hitesh",
+    age: 22,
+ 
+}
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof anotherId);
