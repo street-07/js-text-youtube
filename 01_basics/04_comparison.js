c02250ed3f7e4861713175a0c0429d2a828e19b0
street-07 <ujjwalmehta6969@gmail.com>
2025-04-01 18:25:50 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,31 @@
+// console.log(2>1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1); // it convert
+
+console.log(null > 0);  // it convert into 0
+console.log(null == 0);  // not convert into 0
+console.log(null >= 0);  // convrt into 0
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 3equal then not convert
+
+console.log("2" === 2); 
+
+
+
+
+
+
+
+
+
+
+
+
